214aeb0a25a77ec8e0bf88d9aadf099e8f60874a
zhangshengjie <z.cejay@gmail.com>
2022-06-01 00:45:52 +0800

    v0.0.1

--- test/test.sol
+++ test/test.sol
@@ -1,18 +1,3 @@
-# Escape String
----
-
-Used to escape special characters when splicing json strings in solidity
-
-## Installation
----
-```shell
-npm install --save solidity-escapestring
-```
-
-## Usages
----
-Escape String
-```solidity
 pragma solidity ^0.8.4;
 
 import "solidity-escapestring/escapeString.sol";
@@ -28,8 +13,3 @@ contract HelloWorld {
         return json;
     }
 }
-```
-
-## Author
----
-reloop.eth  [z.cejay@gmail.com]
